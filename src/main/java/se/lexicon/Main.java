fee3a02b593ad7f35d7ec3f63fd95dd0b3e073f9
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 11:54:59 +0200

    Implemented and tested examine().

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,29 @@
 package se.lexicon;
 
+import se.lexicon.model.Beverage;
+import se.lexicon.model.Candy;
+import se.lexicon.model.Tobacco;
+import se.lexicon.model.TobaccoType;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Implementing and printing out a new Candy-item:
+        Candy polkagris = new Candy(19.99, "Polkagris", false);
+        System.out.println(polkagris);
+        System.out.println(polkagris.examine());
+
+        // Implementing and printing out a new Beverage-item:
+        Beverage cocacolazero = new Beverage(29.00, "Coca Cola Zero", true);
+        System.out.println(cocacolazero);
+        System.out.println(cocacolazero.examine());
+
+        // Implementing and printing out a new Tobacco-item:
+        Tobacco ettanlös = new Tobacco(79.99, "Ettan Lös", TobaccoType.Snuff);
+        System.out.println(ettanlös);
+        System.out.println(ettanlös.examine());
+
+
+
     }
 }
