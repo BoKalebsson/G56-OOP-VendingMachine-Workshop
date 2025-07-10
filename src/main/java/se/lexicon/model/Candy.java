df2856d9d91609ede713d9e4385040440f3f5db9
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 10:34:54 +0200

    Implemented attributes, getters, setters and operations.

--- src/main/java/se/lexicon/model/Candy.java
+++ src/main/java/se/lexicon/model/Candy.java
@@ -1,4 +1,46 @@
 package se.lexicon.model;
 
-public class Candy {
+public class Candy extends Product {
+
+    // Attributes:
+    private boolean isSugarFree;
+
+    // Getter:
+    public boolean isSugarFree() {
+        return isSugarFree;
+    }
+
+    // Setter:
+    public void setSugarFree(boolean sugarFree) {
+        isSugarFree = sugarFree;
+    }
+
+    // Constructor:
+    public Candy(double price, String productName, boolean isSugarFree) {
+        super(price, productName);
+        this.isSugarFree = isSugarFree;
+    }
+
+    // Operations:
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-- Product Information --").append("\n");
+        sb.append("Id: ").append(getId()).append("\n");
+        sb.append("Price: ").append(getPrice()).append("\n");
+        sb.append("Product Name: ").append(getProductName()).append("\n");
+        sb.append("Sugarfree: ").append(isSugarFree()).append("\n");
+        sb.append("---------------------------").append("\n");
+        return sb.toString();
+    }
+
+    public String examine(){
+
+        return "";
+    }
+
+    public String use(){
+
+        return "";
+    }
 }
