fb76082dd9f69bc8b9d3f08924a3d10bdaf33a28
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 10:45:46 +0200

    Implemented attributes, getters, setters and operations.

--- src/main/java/se/lexicon/model/Beverage.java
+++ src/main/java/se/lexicon/model/Beverage.java
@@ -1,4 +1,46 @@
 package se.lexicon.model;
 
-public class Beverage {
+public class Beverage extends Product {
+
+    // Attributes:
+    private boolean isAlcoholFree;
+
+    // Getter:
+    public boolean isAlcoholFree() {
+        return isAlcoholFree;
+    }
+
+    // Setter:
+    public void isAlcoholFree(boolean sugarFree) {
+        this.isAlcoholFree = sugarFree;
+    }
+
+    // Constructor:
+    public Beverage(double price, String productName, boolean isAlcoholFree) {
+        super(price, productName);
+        this.isAlcoholFree = isAlcoholFree;
+    }
+
+    // Operations:
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-- Product Information --").append("\n");
+        sb.append("Id: ").append(getId()).append("\n");
+        sb.append("Price: ").append(getPrice()).append("\n");
+        sb.append("Product Name: ").append(getProductName()).append("\n");
+        sb.append("Alcohol free: ").append(isAlcoholFree()).append("\n");
+        sb.append("---------------------------").append("\n");
+        return sb.toString();
+    }
+
+    public String examine(){
+
+        return "";
+    }
+
+    public String use(){
+
+        return "";
+    }
 }
