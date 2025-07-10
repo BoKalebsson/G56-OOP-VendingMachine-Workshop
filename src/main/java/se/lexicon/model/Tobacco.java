948a9f9f313b08bedc319d832aac77a5da36c75c
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 11:05:31 +0200

    Implemented attributes, getters, setters and constructor.

--- src/main/java/se/lexicon/model/Tobacco.java
+++ src/main/java/se/lexicon/model/Tobacco.java
@@ -1,4 +1,46 @@
 package se.lexicon.model;
 
-public class Tobacco {
+public class Tobacco extends Product {
+
+    // Attributes:
+    private TobaccoType tobaccoType;
+
+    // Getter:
+    public TobaccoType getTobaccoType() {
+        return tobaccoType;
+    }
+
+    // Setter:
+    public void setTobaccoType(TobaccoType tobaccoType) {
+        this.tobaccoType = tobaccoType;
+    }
+
+    // Constructor:
+    public Tobacco(double price, String productName) {
+        super(price, productName);
+        setTobaccoType(tobaccoType);
+    }
+
+    // Operations:
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-- Product Information --").append("\n");
+        sb.append("Id: ").append(getId()).append("\n");
+        sb.append("Price: ").append(getPrice()).append("\n");
+        sb.append("Product Name: ").append(getProductName()).append("\n");
+        sb.append("Tobacco Type: ").append(getTobaccoType()).append("\n");
+        sb.append("---------------------------").append("\n");
+        return sb.toString();
+    }
+
+    public String examine(){
+
+        return "";
+    }
+
+    public String use(){
+
+        return "";
+    }
 }
