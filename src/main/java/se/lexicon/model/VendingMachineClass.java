61e967896c2f388f557dbce3c793039264ae2a7c
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 13:47:40 +0200

    Implemented the class VendingMachineClass.

--- src/main/java/se/lexicon/model/VendingMachineClass.java
+++ src/main/java/se/lexicon/model/VendingMachineClass.java
@@ -1,4 +1,51 @@
 package se.lexicon.model;
 
-public class VendingMachineClass {
+public class VendingMachineClass implements VendingMachine {
+
+    // Attributes:
+    Product[] products;
+    int depositPool;
+
+    // Constructor:
+    public VendingMachineClass(int depositPool, Product[] products) {
+        this.depositPool = depositPool;
+        this.products = products;
+    }
+
+    // Getters:
+
+
+    // Setters:
+
+
+    // Operations:
+    @Override
+    public void addCurrency(int amount) {
+
+    }
+
+    @Override
+    public int getBalance() {
+        return depositPool;
+    }
+
+    @Override
+    public Product request(int id) {
+        return null;
+    }
+
+    @Override
+    public int endSession() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription(int id) {
+        return "";
+    }
+
+    @Override
+    public String[] getProducts() {
+        return new String[0];
+    }
 }
