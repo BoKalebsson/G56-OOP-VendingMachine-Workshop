4043e0a8b5c615e0766ddfd642fb11b721afb247
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 13:09:05 +0200

    Implemented methods for interface VendingMachine.

--- src/main/java/se/lexicon/model/VendingMachine.java
+++ src/main/java/se/lexicon/model/VendingMachine.java
@@ -1,4 +1,17 @@
 package se.lexicon.model;
 
 public interface VendingMachine {
+
+    void addCurrency(int amount);
+
+    int getBalance();
+
+    Product request(int id);
+
+    int endSession();
+
+    String getDescription(int id);
+
+    String[] getProducts();
+
 }
