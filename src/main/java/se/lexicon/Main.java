c7e7b47a4440485d95905391dab3fde00bc848b2
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-10 15:31:29 +0200

    Simulated testing ov isValid enum.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,9 +1,6 @@
 package se.lexicon;
 
-import se.lexicon.model.Beverage;
-import se.lexicon.model.Candy;
-import se.lexicon.model.Tobacco;
-import se.lexicon.model.TobaccoType;
+import se.lexicon.model.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,6 +19,16 @@ public class Main {
         Tobacco ettanlös = new Tobacco(79.99, "Ettan Lös", TobaccoType.Snuff);
         System.out.println(ettanlös);
         System.out.println(ettanlös.examine());
+
+        // Simulating different scenarios for testing enum isValid:
+        System.out.println("Is 20 valid? " + ValidAmount.isValid(20));
+        System.out.println("Is 7 valid? " + ValidAmount.isValid(7));
+
+        // Printing out all valid amounts:
+        System.out.println("All valid amounts:");
+        for (ValidAmount amount : ValidAmount.values()) {
+            System.out.println(amount + " = " + amount.getValue());
+        }
 
 
 
